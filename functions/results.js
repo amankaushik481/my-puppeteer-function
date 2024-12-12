5b3c945d08d402bd3fc30ec1ba431c93693f55e6
Steven Irby <email@stevenirby.me>
2024-12-11 21:43:26 -0800

    Fix the other file

--- functions/results.js
+++ functions/results.js
@@ -1,5 +1,4 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+import puppeteer from "puppeteer";
 
 exports.handler = async function(event, context) {
   const browser = await puppeteer.launch({
